f7938198e3827a5bccec522a15b197cbfddeff56
Alan Tubert Perez <alantubert09@gmail.com>
2025-02-07 02:00:05 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Tarea 3',
+  webDir: 'dist'
+};
+
+export default config;
